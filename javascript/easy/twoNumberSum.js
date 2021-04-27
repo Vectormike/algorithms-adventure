8d1ccb9d88575282f185066d17742ae330c40cea
Vectormike <victorjonah199@gmail.com>
2021-04-27 07:14:31 +0100

    ...

--- javascript/easy/twoNumberSum.js
+++ javascript/easy/twoNumberSum.js
@@ -12,6 +12,21 @@ const twoNumberSum = (array, targetSum) => {
   return [];
 };
 
+// 0(n) time & 0(n) space
+
+// Using the Hash table (nums = {})
+const twoNumberSum = (array, targetSum) => {
+  const nums = {};
+  for (const num of array) {
+    if (targetSum - num in nums) {
+      return [targetSum - num, num];
+    } else {
+      nums[num] = true;
+    }
+  }
+  return [];
+};
+
 const array = [3, 5, -4, 8, 11, 1, -1, 6];
 const targetSum = 10;
 
